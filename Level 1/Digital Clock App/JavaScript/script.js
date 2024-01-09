89bf8584763cfbc9e422439800cbebc17f804270
Thahir BAH <thahirbah@gmail.com>
2024-01-09 08:36:57 +0100

    digital clock app updated

--- Level 1/Digital Clock App/JavaScript/script.js
+++ Level 1/Digital Clock App/JavaScript/script.js
@@ -1,8 +1,21 @@
-const d = new Date();
-let hour = d.getHours();
-let minutes = d.getMinutes();
-let seconds = d.getSeconds();
+const date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth();
+let day = date.getDate();
+let hour = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
 
 document.getElementById("hour").innerHTML = hour;
 document.getElementById("minute").innerHTML = minutes;
 document.getElementById("second").innerHTML = seconds;
+document.getElementById("year").innerHTML = year;
+document.getElementById("month").innerHTML = months[month];
+document.getElementById("day").innerHTML = day;
+document.getElementById("weekday").innerHTML = weekday[day]
+
+
